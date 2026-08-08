ed5aa58d17c5cb11075baa984143210898bd4329
Er. Gurbhej Gill <gurbhejgill1996@gmail.com>
2026-08-09 09:55:36 +1200

    Added the changes for add more six values in existing array

---
+++ Week2_Activity2/Week2_Activity3/Week2_Activity4/Testing_slices.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    my_list = [1, 3, 5, 7, 9, 11]
+
+    my_list[2:4] = [-3, -9, -11, -13]
+    print(my_list)
+    my_list[6:5] = [-1, -89, -56, -33, -44, -55]
+
+    print(my_list)
